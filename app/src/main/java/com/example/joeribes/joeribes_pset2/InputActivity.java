18c0f521cd43c8f6e0e0b54121dfcc0ad69525a6
Joeri Bes <joeri.bes@student.uva.nl>
2017-09-13 13:14:56 +0200

    - Hints

--- app/src/main/java/com/example/joeribes/joeribes_pset2/InputActivity.java
+++ app/src/main/java/com/example/joeribes/joeribes_pset2/InputActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,6 +15,8 @@ import java.io.Serializable;
 public class InputActivity extends AppCompatActivity {
 
     Story story;
+    String hint;
+    int wordCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +24,39 @@ public class InputActivity extends AppCompatActivity {
         setContentView(R.layout.activity_input);
 
         story = (Story) getIntent().getSerializableExtra("story");
+        wordCounter();
+        setHintWord();
 
     }
+
+    public void setHintWord() {
+        hint = story.getNextPlaceholder();
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setHint(hint);
+
+        TextView hintTextPhrase = (TextView) findViewById(R.id.hintTextView);
+        hintTextPhrase.setHint(" please type a/an " + hint);
+
+    }
+
+
+
 
     public void fillInWord(View view) {
         EditText editText = (EditText) findViewById(R.id.editText);
         String word = editText.getText().toString();
-
         story.fillInPlaceholder(word);
+        editText.setText("");
+        wordCounter();
+        setHintWord();
 
+    }
+
+    public void wordCounter() {
+        wordCount = story.getPlaceholderRemainingCount();
+
+        TextView madLib = (TextView) findViewById(R.id.wordCounterView);
+        madLib.setHint(String.valueOf(wordCount) + " word(s) left");
     }
 
 
